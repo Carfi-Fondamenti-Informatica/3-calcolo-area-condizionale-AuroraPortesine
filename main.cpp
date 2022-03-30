fe92be42078643f42ebbd25d90bfd18cf02289e6
AuroraPortesine <100139517+AuroraPortesine@users.noreply.github.com>
2022-03-30 15:47:20 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,17 +8,14 @@ int main() {
     cin >> opzione;
     switch(opzione){
         case 0:
-            cout << "Inserire il valore della base e dell'altezza\n";
             cin >> a >> b;
             cout<< (Area=(a*b)/2);
             break;
         case 1:
-            cout << "Inserire il valore del lato\n";
             cin >> a;
             cout<< (Area=a*a);
             break;
         case 2:
-            cout << "Inserire rispettivamente il valore del primo e del secondo lato\n";
             cin >> a >> b;
             cout<< (Area=a*b);
             break;
