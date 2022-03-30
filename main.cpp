b4c5653254de1cf63410095668c50dc48213d743
AuroraPortesine <100139517+AuroraPortesine@users.noreply.github.com>
2022-03-30 15:20:34 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,28 @@
 using namespace std;
 
 int main() {
-   return 0;
+    float a=0, b=0;
+    int opzione=0, Area;
+    cout << "Scegliere l'opzione desiderata\n";
+    cin >> opzione;
+    switch(opzione){
+        case 0:
+            cout << "Inserire il valore della base e dell'altezza\n";
+            cin >> a >> b;
+            cout<< (Area=(a*b)/2);
+            break;
+        case 1:
+            cout << "Inserire il valore del lato\n";
+            cin >> a;
+            cout<< (Area=a*a);
+            break;
+        case 2:
+            cout << "Inserire rispettivamente il valore del primo e del secondo lato\n";
+            cin >> a >> b;
+            cout<< (Area=a*b);
+            break;
+        default:
+            cout<<"Opzione non valida\n";
+    }
+    return 0;
 }
