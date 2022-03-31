fd36deee758528b82ff6a8f6173b368464a5fb4d
AuroraPortesine <100139517+AuroraPortesine@users.noreply.github.com>
2022-03-31 10:45:07 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,7 @@ using namespace std;
 int main() {
     float a=0, b=0;
     int opzione=0;
-    cin >> opzione;
-    cin >> a >> b;
+    cin >> a >> b >> opzione;
     switch(opzione){
         case 0:
             cout << (a*b)/2 << endl;
